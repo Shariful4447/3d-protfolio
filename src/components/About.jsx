bde9be72ed02d655c005b3a24e7bc1be51e799c5
Shariful4447 <shariful.islam4447@gmail.com>
2023-12-29 20:51:11 +0100

    about section updated but need to update little bit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,10 +35,20 @@ const ServiceCard = ({ index, title, icon }) => (
   //     </div>
   //   </motion.div>
   // </Tilt>
-  <Tilt>
-      <div style={{ height: '300px', backgroundColor: 'white' }}>
-        <h1>React Parallax Tilt 👀</h1>
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+        >
+      <div className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col' style={{ height: '200px',width:'250px', backgroundColor: 'white' }}>
+      <img
+         src={icon}
+         alt='web-development'
+        className='w-16 h-16 object-contain'
+      />
+        <h1 className='text-blue text-[20px] font-bold text-center'>{title}</h1>
       </div>
+    </motion.div>  
     </Tilt>
 );
 
