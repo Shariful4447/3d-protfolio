d2f4d74a1a5343625aef9598de0272c3db4826b9
Shariful4447 <shariful.islam4447@gmail.com>
2023-12-29 20:40:47 +0100

    tilt added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Tilt } from "react-tilt";
+
+import Tilt from 'react-parallax-tilt';
 // import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
@@ -8,33 +9,38 @@ import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
-// const ServiceCard = ({ index, title, icon }) => (
-//   // <Tilt className='xs:w-[250px] w-full'>
-//   //   <motion.div
-//   //     variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-//   //     className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-//   //   >
-//   //     <div
-//   //       options={{
-//   //         max: 45,
-//   //         scale: 1,
-//   //         speed: 450,
-//   //       }}
-//   //       className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-//   //     >
-//   //       <img
-//   //         src={icon}
-//   //         alt='web-development'
-//   //         className='w-16 h-16 object-contain'
-//   //       />
+const ServiceCard = ({ index, title, icon }) => (
+  // <Tilt className='xs:w-[250px] w-full'>
+  //   <motion.div
+  //     variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+  //     className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+  //   >
+  //     <div
+  //       options={{
+  //         max: 45,
+  //         scale: 1,
+  //         speed: 450,
+  //       }}
+  //       className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+  //     >
+  //       <img
+  //         src={icon}
+  //         alt='web-development'
+  //         className='w-16 h-16 object-contain'
+  //       />
 
-//   //       <h3 className='text-white text-[20px] font-bold text-center'>
-//   //         {title}
-//   //       </h3>
-//   //     </div>
-//   //   </motion.div>
-//   // </Tilt>
-// );
+  //       <h3 className='text-white text-[20px] font-bold text-center'>
+  //         {title}
+  //       </h3>
+  //     </div>
+  //   </motion.div>
+  // </Tilt>
+  <Tilt>
+      <div style={{ height: '300px', backgroundColor: 'white' }}>
+        <h1>React Parallax Tilt 👀</h1>
+      </div>
+    </Tilt>
+);
 
 const About = () => {
   return (
@@ -55,11 +61,11 @@ const About = () => {
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
-      {/* <div className='mt-20 flex flex-wrap gap-10'>
+      <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
-      </div> */}
+      </div>
     </>
   );
 };
