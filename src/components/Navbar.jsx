d2489948c7be2bcb7730be7694408e2462318a60
Shariful4447 <shariful.islam4447@gmail.com>
2023-12-29 18:57:50 +0100

    Navbar element added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import {logo, menu, close } from '../assets';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
