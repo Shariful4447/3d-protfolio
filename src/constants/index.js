c8f1990b3b5784b0f6dc957822b386d3bc74b764
Shariful4447 <shariful.islam4447@gmail.com>
2024-01-13 11:10:38 +0100

    work section update added project

--- src/constants/index.js
+++ src/constants/index.js
@@ -242,7 +242,7 @@ const projects = [
     live_link: "https://donation-collecter.netlify.app/",
   },
   {
-    name: "Job IT",
+    name: "Carrer Hub",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
     tags: [
